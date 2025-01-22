746ddbe004935570ac73946433e9227b439d3bdf
Andrew R. McCluskey <andrew.mccluskey@bristol.ac.uk>
2025-01-22 10:06:12 +0000

    Ticks

--- src/scripts/Fig2.py
+++ src/scripts/Fig2.py
@@ -44,7 +44,6 @@ xmin = 4
 xmax = 16
 s_set = 50
 
-
 # Tick removal, horizontal line plotting
 for x in [ax1]:
     x.set_xlim(4,16.1)
@@ -56,7 +55,6 @@ for x in [ax1]:
     x.set_xticks([])
     x.axis('off')
 
-
 # Selecting for specific molecule
 particles = 3
 coords = np.array([xrange[0],xrange[-1],xrange[20]])
@@ -66,11 +64,10 @@ ax1.scatter(coords,np.zeros(particles), s = s_set, color = ['black','black','#01
 ax1.plot(coords,np.zeros(particles), color = 'grey', alpha = 0.8, linewidth = l_set)
 ax1.scatter(MDA_store[20],0, s=s_set, color = '#029E73', marker = 'x', label = 'Naive and Molecule Centre of Mass',zorder=10, alpha=0.8,linewidths=cross_size)
 ax1.scatter(pi_store[20],0, s=s_set, color = '#D55E00', marker = 'x', label = 'Naive and Molecule Centre of Mass',zorder=10, alpha=0.8,linewidths=cross_size)
-
+ax1.set_yticks([0, 0.1, 0.2, 0.3])
+ax1.set_xticks([0.25, 0.5, 0.75])
 
 ax0 = fig.add_subplot(gs[1,0])
-
-
 
 ax0.plot(xrange/20,np.abs(pi_store - MDA_store)/10, label = 'moving particle', color = '#0173B2',solid_capstyle='round')
 ax0.set_xlabel('Particle coordinates')
@@ -78,16 +75,12 @@ ax0.set_ylabel('Centre of mass error / Span of molecule')
 ax0.set_ylim(0,0.35)
 ax0.set_xlim(0.2,0.8)
 
-
-
 # Demonstration line
 ax1.axvline(xrange[20], ymax = 0.5, color = 'orange', linestyle = 'dashed')
 ax0.axvline(xrange[20]/20-0.0013, ymin = (np.abs(pi_store - MDA_store)/10)[20]/0.35, color = 'orange', linestyle = 'dashed')
 ax0.axhline((np.abs(pi_store - MDA_store)/10)[20], xmax = 0.25, color = 'orange', linestyle = 'dashed', label = 'example error')
 ax0.scatter(-20,20, s=s_set, color = 'orange', marker = 'x', label = 'B&B COM',zorder=10, alpha=0.8,linewidths=cross_size)
 ax0.scatter(-20,-20, s=s_set, color = '#029E73', marker = 'x', label = 'True COM',zorder=10, alpha=0.8,linewidths=cross_size)
-
-
 
 ax0.spines['top'].set_visible(False)
 plt.savefig(paths.figures / 'Fig2.pdf', bbox_inches='tight', pad_inches = 0)
