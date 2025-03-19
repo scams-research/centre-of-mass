c47de2348f93ad34220b7def15a3bee70c1df4f4
Harry-Rich <bb24144@bristol.ac.uk>
2025-03-19 17:56:45 +0000

    Updated Plotting files

--- src/scripts/Fig3.py
+++ src/scripts/Fig3.py
@@ -1,79 +1,49 @@
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from tqdm import tqdm
-from scipy.stats import uniform
-import matplotlib as matplotlib
-from Functions import pi_centrer
+from Functions import pi_centrer,frechet_statistic,intrinsic_mean
+from Variables import font_size
+
 
 # Main
-m = int(2 ** 24) 
-k = np.zeros((m, 6))
+m = int(2 ** 19) 
+k = np.zeros((m, 4))
 x_max = 1
 x_min = 0
 
-# Variables to define wrapping
-minimum_uniform = 0.65
 
+k = np.zeros((m, 4))
 
-for i in tqdm(range(m)):
+for i in range(m):
     rng = np.random.default_rng(i)
+    minimum_uniform = rng.uniform(0,1,size=1)
     N = rng.integers(3, 512, size=1)
-    n = rng.uniform(0.1, 0.45, size=1)
+    n = rng.uniform(0.1, 0.59, size=1)
     particles = rng.uniform(minimum_uniform, minimum_uniform+n, size=N)
     y = particles
     particle_span = particles.max() - particles.min()
     y_pbc = y % x_max
     k[i, 0] = N
     k[i, 1] = pi_centrer(y_pbc, np.ones_like(y_pbc), x_min, x_max)
-    k[i, 2] = np.average(y)
+    k[i, 2] = intrinsic_mean(y_pbc,np.ones_like(y_pbc))
     offset = (k[i, 1] - ((x_max - x_min) / 2))
     moved = (y_pbc - offset) % (x_max - x_min)
-    k[i, 3] = np.average(moved) + offset
-    offset = (k[i, 1] - ((x_max - x_min) / 2))
-    moved = (y - offset) % (x_max - x_min)
-    # aymmetry metric
-    y_hist, x_hist = np.histogram(moved, bins=np.linspace(x_min, x_max, 101), density=True)
-    asymmetry = y_hist[:int(y_hist.size / 2)] - y_hist[int(y_hist.size / 2):][::-1]
-    k[i, 4] = np.sum(np.abs(asymmetry))
-    k[i,5] = particle_span
+    k[i, 3] = (np.average(moved) + offset) % 1
 
-# Unwrapping Centre of mass if required to allow for comparison with true Centre of mass
-k[:, 1][np.where(k[:, 1] < (x_max - x_min) * 0.5)] += x_max
-k[:, 2][np.where(k[:, 2] < (x_max - x_min) * 0.5)] += x_max
-k[:, 3][np.where(k[:, 3] < (x_max - x_min) * 0.5)] += x_max
+# Plotting
+matplotlib.rcParams.update({'font.size': font_size})
+fig, axis = plt.subplots(figsize = (4.03, 4.03))
 
-
-# Binning Bai and Breen error for visulation 
-N_bins = 100
-bins = np.logspace(-1, 2, N_bins)
-binned_indices = np.digitize(k[:, 4], bins)
-
-error_bb_array = np.empty([len(bins),2000000])
-error_bb_array[:] = np.nan
-err = (k[:, 1] - k[:, 2])/k[:,5]
-for z in range(0,N_bins):
-    length = len(np.where(binned_indices == z)[0])
-    if length != 0:
-        error_bb_array[z,0:length] = err[np.where(binned_indices == z)[0]]
-
-
+num_bins = 100
 credible_intervals = [[16, 84], [2.5, 97.5], [0.15, 99.85]]
 alpha = [0.6, 0.4, 0.2]
-matplotlib.rcParams.update({'font.size': 7})
-fig, axis = plt.subplots(figsize = (4.03, 3.3))
+axis.set_aspect('equal')
+axis.scatter(k[:,2],k[:,1],alpha = 0.4, color = '#0173B2', rasterized = True )
+axis.plot(k[:,2],k[:,3]%1, color = 'k') #linewidths = 0.001 )
+axis.set_xlabel('Intrinsic mean')
+axis.set_ylabel('Centre of mass')
+axis.set_xlim([0,1])
+axis.set_ylim([0,1])
 
-axis.plot(k[:, 4], (k[:, 3] - k[:, 2])/k[:,5], label='This Work', color = 'orange',solid_capstyle='round')
 
-for i, ci in enumerate(credible_intervals):
-    axis.fill_between(bins,
-                    *np.nanpercentile(error_bb_array,ci, axis=1),
-                    alpha=alpha[i],
-                    color='#0173B2',
-                    label= f'${i+1}\sigma$ - B&B method',
-                    lw=0)
-    
-
-axis.set_xlabel('Asymmetry')
-axis.set_ylabel("Centre of mass error / Span of particles")
-plt.tight_layout()
-plt.show()
+fig.savefig("method valid.pdf", bbox_inches='tight', pad_inches=0)
