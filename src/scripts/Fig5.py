eb0a0485f5928bcce54822990bbd5a9f6dd4e798
Andrew R. McCluskey <andrew.mccluskey@bristol.ac.uk>
2025-03-20 14:27:44 +0000

    Fixing figures

--- src/scripts/Fig5.py
+++ src/scripts/Fig5.py
@@ -8,6 +8,9 @@ import paths
 
 k = np.loadtxt(paths.data / 'sampling.txt')
 
+x_max = 1
+x_min = 0
+
 # Unwrapping Centre of mass if required to allow for comparison with true Centre of mass
 k[:, 1][np.where(k[:, 1] < (x_max - x_min) * 0.5)] += x_max
 k[:, 2][np.where(k[:, 2] < (x_max - x_min) * 0.5)] += x_max
