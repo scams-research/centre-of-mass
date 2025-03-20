e5c2d81cd2511f1967033396f2db53249b4b26a9
Andrew R. McCluskey <andrew.mccluskey@bristol.ac.uk>
2025-03-20 12:04:38 +0000

    Move the sampling code

--- src/scripts/Fig5.py
+++ src/scripts/Fig5.py
@@ -6,43 +6,7 @@ import matplotlib as matplotlib
 from Functions import pi_centrer
 import paths
 
-# Main
-m = int(2 ** 24) 
-k = np.zeros((m, 6))
-x_max = 1
-x_min = 0
-
-# Variables to define wrapping
-minimum_uniform = 0.65
-
-
-for i in tqdm(range(m)):
-    rng = np.random.default_rng(i)
-    N = rng.integers(3, 512, size=1)
-    n = rng.uniform(0.1, 0.45, size=1)
-    particles = rng.uniform(minimum_uniform, minimum_uniform+n, size=N)
-    y = particles
-    particle_span = particles.max() - particles.min()
-    y_pbc = y % x_max
-    k[i, 0] = N
-    k[i, 1] = pi_centrer(y_pbc, np.ones_like(y_pbc), x_min, x_max)
-    k[i, 2] = np.average(y)
-    offset = (k[i, 1] - ((x_max - x_min) / 2))
-    moved = (y_pbc - offset) % (x_max - x_min)
-    k[i, 3] = np.average(moved) + offset
-    offset = (k[i, 1] - ((x_max - x_min) / 2))
-    moved = (y - offset) % (x_max - x_min)
-    # aymmetry metric
-    y_hist, x_hist = np.histogram(moved, bins=np.linspace(x_min, x_max, 101), density=True)
-    asymmetry = y_hist[:int(y_hist.size / 2)] - y_hist[int(y_hist.size / 2):][::-1]
-    k[i, 4] = np.sum(np.abs(asymmetry))
-    k[i,5] = particle_span
-
-# Unwrapping Centre of mass if required to allow for comparison with true Centre of mass
-k[:, 1][np.where(k[:, 1] < (x_max - x_min) * 0.5)] += x_max
-k[:, 2][np.where(k[:, 2] < (x_max - x_min) * 0.5)] += x_max
-k[:, 3][np.where(k[:, 3] < (x_max - x_min) * 0.5)] += x_max
-
+k = np.load(paths.data / 'Fig5.npy')
 
 # Binning Bai and Breen error for visulation 
 N_bins = 100
